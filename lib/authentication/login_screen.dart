eeaf7d4823a6ab1e9eaab7950b6fa2217bda0757
Sk. Nahid <nahidsheikh2001@gmail.com>
2024-03-06 01:05:18 +0600

    login_signup_page_done

--- lib/authentication/login_screen.dart
+++ lib/authentication/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uber_clone_user_app/authentication/signup_screen.dart';
 
 class LogInScreen extends StatefulWidget {
   const LogInScreen({super.key});
@@ -8,8 +9,94 @@ class LogInScreen extends StatefulWidget {
 }
 
 class _LogInScreenState extends State<LogInScreen> {
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/logo.png"
+                ),
+               const Text(
+                  "Log In Here",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(22),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: emailTextEditingController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          labelText: "User E-mail",
+                          labelStyle: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        ),
+                      ),const SizedBox(height: 22,),
+
+                      TextField(
+                        controller: passwordTextEditingController,
+                        obscureText: true,
+                        keyboardType: TextInputType.visiblePassword,
+                        decoration: const InputDecoration(
+                          labelText: "User Password",
+                          labelStyle: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        ),
+                      ),const SizedBox(height: 22,),
+
+                      ElevatedButton(
+                        onPressed: () 
+                        {
+
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.purple,
+                          padding:const EdgeInsets.symmetric(
+                            horizontal: 80,
+                            vertical: 10,
+                          )
+                        ),
+                        child: const Text("Log In"),
+                        )
+                    ],
+                  ),
+                  ),
+
+                  TextButton(
+                    onPressed: () 
+                    {
+                      Navigator.push(context, MaterialPageRoute(builder: (c)=> const SignUpScreen()));
+                    },
+                    child: const Text(
+                      "Don't have any account? Sign Up here",
+                    ),
+                  )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/authentication/signup_screen.dart
+++ lib/authentication/signup_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uber_clone_user_app/authentication/login_screen.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -9,6 +10,9 @@ class SignUpScreen extends StatefulWidget {
 
 class _SignUpScreenState extends State<SignUpScreen> 
 {
+  TextEditingController userNameTextEditingController = TextEditingController();
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) 
   {
@@ -21,12 +25,95 @@ class _SignUpScreenState extends State<SignUpScreen>
               Image.asset(
                 "assets/images/logo.png"
                 ),
-            ],
+               const Text(
+                  "Sign Up Here",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
 
+                Padding(
+                  padding: const EdgeInsets.all(22),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: userNameTextEditingController,
+                        keyboardType: TextInputType.text,
+                        decoration: const InputDecoration(
+                          labelText: "User Name",
+                          labelStyle: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        ),
+                      ),const SizedBox(height: 22,),
+
+                      TextField(
+                        controller: emailTextEditingController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          labelText: "User E-mail",
+                          labelStyle: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        ),
+                      ),const SizedBox(height: 22,),
+
+                      TextField(
+                        controller: passwordTextEditingController,
+                        obscureText: true,
+                        keyboardType: TextInputType.visiblePassword,
+                        decoration: const InputDecoration(
+                          labelText: "User Password",
+                          labelStyle: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        ),
+                      ),const SizedBox(height: 22,),
+
+                      ElevatedButton(
+                        onPressed: () 
+                        {
+
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.purple,
+                          padding:const EdgeInsets.symmetric(
+                            horizontal: 80,
+                            vertical: 10,
+                          )
+                        ),
+                        child: const Text("Sign Up"),
+                        )
+                    ],
+                  ),
+                  ),
+
+                  TextButton(
+                    onPressed: () 
+                    {
+                      Navigator.push(context, MaterialPageRoute(builder: (c)=> const LogInScreen()));
+                    },
+                    child: const Text(
+                      "Already have an account? Login Here",
+                    ),
+                  )
+            ],
           ),
         ),
       ),
-
     );
   }
 }
