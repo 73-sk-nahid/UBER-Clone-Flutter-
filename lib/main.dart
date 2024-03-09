e8dba5d34a382d6d21573c1ba7615321accda61a
Sk. Nahid <nahidsheikh2001@gmail.com>
2024-03-10 00:21:21 +0600

    Firebase Connection Problem

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:uber_clone_user_app/authentication/login_screen.dart';
 
-Future<void> main() async
+/* Future<void> main() async
 {
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp();
 
+  runApp(const MyApp());
+} */
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(const MyApp());
 }
 
