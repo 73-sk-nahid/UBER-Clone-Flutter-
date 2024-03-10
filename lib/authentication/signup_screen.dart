e9c998cc72af7e67fc074e92d3ad2e3216e4dd29
Sk. Nahid <nahidsheikh2001@gmail.com>
2024-03-10 22:25:54 +0600

    solve signup

--- lib/authentication/signup_screen.dart
+++ lib/authentication/signup_screen.dart
@@ -171,108 +171,75 @@ class _SignUpScreenState extends State<SignUpScreen> {
     // ignore: prefer_interpolation_to_compose_strings
     if (userNameTextEditingController.text.trim().length < 3) {
       cMethods.displaySnackBar("Username must be 4 letter", context);
-    }
-    else if(!RegExp(r'^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$').hasMatch(emailTextEditingController.text.trim())){
+    } else if (!RegExp(r'^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$')
+        .hasMatch(emailTextEditingController.text.trim())) {
       cMethods.displaySnackBar("Enter valid email address", context);
-    }
-    else if(phoneNumberTextEditingController.text.trim().length < 10) {
+    } else if (phoneNumberTextEditingController.text.trim().length < 10) {
       cMethods.displaySnackBar("Phone number must be 11 digit", context);
-    }
-    else if(passwordTextEditingController.text.trim().length < 5){
+    } else if (passwordTextEditingController.text.trim().length < 5) {
       cMethods.displaySnackBar("Password must be 6 characters", context);
-    }
-    else if(!passwordTextEditingController.text.trim().contains(RegExp(r'[A-Z]'))){
+    } else if (!passwordTextEditingController.text
+        .trim()
+        .contains(RegExp(r'[A-Z]'))) {
       cMethods.displaySnackBar("Must use Uppercase", context);
-    }
-    else if(!passwordTextEditingController.text.trim().contains(RegExp(r'[a-z]'))){
+    } else if (!passwordTextEditingController.text
+        .trim()
+        .contains(RegExp(r'[a-z]'))) {
       cMethods.displaySnackBar("Must use Lowercase", context);
-    }
-    else if(!passwordTextEditingController.text.trim().contains(RegExp(r'[0-9]'))){
+    } else if (!passwordTextEditingController.text
+        .trim()
+        .contains(RegExp(r'[0-9]'))) {
       cMethods.displaySnackBar("Must use Digit", context);
-    }
-    else if(!passwordTextEditingController.text.trim().contains(RegExp(r'[!@#%^&*(),.?":{}|<>]'))){
+    } else if (!passwordTextEditingController.text
+        .trim()
+        .contains(RegExp(r'[!@#%^&*(),.?":{}|<>]'))) {
       cMethods.displaySnackBar("Must use Special Character", context);
-    }
-    else
-    {
+    } else {
       registerUser();
     }
   }
 
   void registerUser() async {
-  try {
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) =>
-          const LoadingDialog(messageText: "Registering your account.."),
-    );
+    try {
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) =>
+            const LoadingDialog(messageText: "Registering your account.."),
+      );
 
-    final UserCredential userCredential =
-        await FirebaseAuth.instance.createUserWithEmailAndPassword(
-      email: emailTextEditingController.text.trim(),
-      password: passwordTextEditingController.text.trim(),
-    );
-
-    final User? userFirebase = userCredential.user;
-
-    if (userFirebase == null) {
-      cMethods.displaySnackBar("User registration failed.", context);
-      return;
-    }
-
-    Navigator.pop(context);
-
-    DatabaseReference usersRef =
-        FirebaseDatabase.instance.ref().child("users").child(userFirebase.uid);
-    Map userDataMap = {
-      "name": userNameTextEditingController.text.trim(),
-      "email": emailTextEditingController.text.trim(),
-      "phone": "0" + phoneNumberTextEditingController.text.trim(),
-      "id": userFirebase.uid,
-      "blockStatus": "no",
-    };
-
-    usersRef.set(userDataMap);
-    Navigator.push(context, MaterialPageRoute(builder: (c) => HomePage()));
-  } catch (error) {
-    Navigator.pop(context); // Dismiss the loading dialog in case of an error
-    cMethods.displaySnackBar("Error: $error", context);
-  }
-}
-
-
-  /* void registerUser() async {
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) => const LoadingDialog(messageText: "Registering your account.."),
-    );
-
-    final User? userFirebase = (
-    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      final UserCredential userCredential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: emailTextEditingController.text.trim(),
         password: passwordTextEditingController.text.trim(),
-    ).catchError((errorMsg)
-        {
-          cMethods.displaySnackBar(errorMsg.toString(), context);
-        })
-    ).user;
+      );
 
-    if(!context.mounted) return;
-    Navigator.pop(context);
+      final User? userFirebase = userCredential.user;
 
-    DatabaseReference usersRef = FirebaseDatabase.instance.ref().child("users").child(userFirebase!.uid);
-    Map userDataMap = 
-    {
-      "name": userNameTextEditingController.text.trim(),
-      "email": emailTextEditingController.text.trim(),
-      "phone": phoneNumberTextEditingController.text.trim(),
-      "id": userFirebase.uid,
-      "blockStatus": "no",
-    };
+      if (userFirebase == null) {
+        cMethods.displaySnackBar("User registration failed.", context);
+        return;
+      }
 
-    usersRef.set(userDataMap);
-    Navigator.push(context, MaterialPageRoute(builder: (c)=> HomePage()));
-  } */
+      Navigator.pop(context);
+
+      DatabaseReference usersRef = FirebaseDatabase.instance
+          .ref()
+          .child("users")
+          .child(userFirebase.uid);
+      Map userDataMap = {
+        "name": userNameTextEditingController.text.trim(),
+        "email": emailTextEditingController.text.trim(),
+        "phone": "0" + phoneNumberTextEditingController.text.trim(),
+        "id": userFirebase.uid,
+        "blockStatus": "no",
+      };
+
+      usersRef.set(userDataMap);
+      Navigator.push(context, MaterialPageRoute(builder: (c) => HomePage()));
+    } catch (error) {
+      Navigator.pop(context); // Dismiss the loading dialog in case of an error
+      cMethods.displaySnackBar("Error: $error", context);
+    }
+  }
 }
