612e3dcbadf70a6dfa1eb8bf9e3282cd5a6ab60a
Sk. Nahid <nahidsheikh2001@gmail.com>
2024-03-12 12:16:24 +0600

    added gitignore

--- lib/global/global_var.dart
+++ lib/global/global_var.dart
@@ -1,2 +1,9 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 String userName = "";
 String googleMapKey = "AIzaSyDZDqr8Va2drRaWXkCJyddm6BwfmEXJyRg";
+
+const CameraPosition googlePlexInitialPosition = CameraPosition(
+    target: LatLng(23.7168531,90.4941576),
+    zoom: 21,
+  );
