18933270dd3c32539ffc3c198623207d3a84efbc
Sk. Nahid <nahidsheikh2001@gmail.com>
2024-05-22 00:59:54 +0600

    bottom nav done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,6 +9,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:uber_clone_user_app/authentication/login_screen.dart';
 import 'package:uber_clone_user_app/global/global_var.dart';
 import 'package:uber_clone_user_app/methods/common_methods.dart';
+import 'package:uber_clone_user_app/pages/searchPage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -24,6 +25,10 @@ class _HomePageState extends State<HomePage> {
   Position? userCurrentLocation;
   GlobalKey<ScaffoldState> sKey = GlobalKey<ScaffoldState>();
   CommonMethods cMethods = CommonMethods();
+  double searchContainerHeight = 276;
+  double bottomMapPadding = 0;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -106,15 +111,14 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               GestureDetector(
-                onTap: ()
-                {
+                onTap: () {
                   FirebaseAuth.instance.signOut();
-                  Navigator.push(context, MaterialPageRoute(builder: (c) => LogInScreen()));
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (c) => LogInScreen()));
                 },
                 child: ListTile(
                   leading: IconButton(
-                    onPressed: () {
-                      },
+                    onPressed: () {},
                     icon: const Icon(
                       Icons.info,
                       color: Colors.grey,
@@ -134,7 +138,10 @@ class _HomePageState extends State<HomePage> {
         children: [
           //google Map
           GoogleMap(
-            padding: const EdgeInsets.only(top: 30,),
+            padding: EdgeInsets.only(
+              top: 30,
+              bottom: bottomMapPadding,
+            ),
             mapType: MapType.normal,
             myLocationEnabled: true,
             initialCameraPosition: googlePlexInitialPosition,
@@ -143,6 +150,10 @@ class _HomePageState extends State<HomePage> {
 
               googleMapCompleterController.complete(controllerGoogleMap);
 
+              setState(() {
+                bottomMapPadding = 300;
+              });
+
               getUserCurrentLocation();
             },
           ),
@@ -150,36 +161,96 @@ class _HomePageState extends State<HomePage> {
           //drawer button
           Positioned(
             top: 42,
-              left: 19,
-              child: GestureDetector(
-                onTap: (){
-                  sKey.currentState!.openDrawer();
-                },
-                child: Container(
-                  decoration: BoxDecoration(
+            left: 19,
+            child: GestureDetector(
+              onTap: () {
+                sKey.currentState!.openDrawer();
+              },
+              child: Container(
+                decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(20),
-                    boxShadow: const
-                        [
-                          BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 5,
-                            spreadRadius: 0.5,
-                            offset: Offset(0.7, 0.7),
-                          )
-                    ]
-                  ),
-                  child: const CircleAvatar(
-                    backgroundColor: Colors.grey,
-                    radius: 20,
-                    child: Icon(
-                      Icons.menu,
-                      color: Colors.black87,
-                    ),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 5,
+                        spreadRadius: 0.5,
+                        offset: Offset(0.7, 0.7),
+                      )
+                    ]),
+                child: const CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  radius: 20,
+                  child: Icon(
+                    Icons.menu,
+                    color: Colors.black87,
                   ),
                 ),
               ),
+            ),
+          ),
+
+          //search location icon button
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: -80,
+            child: Container(
+              height: searchContainerHeight,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (c)=> SearchDestinationPage()));
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey,
+                        shape:const CircleBorder(),
+                        padding:const EdgeInsets.all(24),
+                      ),
+                      child: const Icon(
+                        Icons.search,
+                        color: Colors.white,
+                        size: 25,
+                      ),
+                  ),
+
+                  ElevatedButton(
+                    onPressed: () {
+
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey,
+                      shape:const CircleBorder(),
+                      padding:const EdgeInsets.all(24),
+                    ),
+                    child: const Icon(
+                      Icons.home,
+                      color: Colors.white,
+                      size: 25,
+                    ),
+                  ),
+
+                  ElevatedButton(
+                    onPressed: () {
+
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey,
+                      shape:const CircleBorder(),
+                      padding:const EdgeInsets.all(24),
+                    ),
+                    child: const Icon(
+                      Icons.work,
+                      color: Colors.white,
+                      size: 25,
+                    ),
+                  ),
+                ],
               ),
+            ),
+          )
         ],
       ),
     );
@@ -211,13 +282,15 @@ class _HomePageState extends State<HomePage> {
           userName = ((snap.snapshot.value as Map)["name"]);
         } else {
           FirebaseAuth.instance.signOut();
-          Navigator.push(context, MaterialPageRoute(builder: (c) => LogInScreen()));
+          Navigator.push(
+              context, MaterialPageRoute(builder: (c) => LogInScreen()));
           cMethods.displaySnackBar(
               "Account Blocked \nContact Customer Care", context);
         }
       } else {
         FirebaseAuth.instance.signOut();
-        Navigator.push(context, MaterialPageRoute(builder: (c) => LogInScreen()));
+        Navigator.push(
+            context, MaterialPageRoute(builder: (c) => LogInScreen()));
       }
     });
   }
